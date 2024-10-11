cbedf99966a199cfdc1c09249dc1617d9103eae0
shandilya1998 <shandilya.shreyas@gmail.com>
2024-10-11 17:29:19 +0530

    commit
    Problem
    =======
    
    Solution
    ========
    
    Note
    ====

--- scripts/build_ros_packages.sh
+++ scripts/build_ros_packages.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 export CMAKE_PREFIX_PATH=/Pangolin/build:$CMAKE_PREFIX_PATH
@@ -9,4 +10,4 @@ echo "***********************************************"
 echo $PWD
 echo "***********************************************"
 # rm -rf build install
-colcon build --symlink-install --cmake-args '-DCMAKE_EXPORT_COMPILE_COMMANDS=1' '-DCMAKE_BUILD_TYPE=Release' '-Wno-dev' --mixin debug
+colcon build --symlink-install --cmake-args '-DCMAKE_EXPORT_COMPILE_COMMANDS=1' '-DCMAKE_BUILD_TYPE=RelWithDebInfo' '-Wno-dev' --mixin debug
